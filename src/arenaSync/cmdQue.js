b4bd62d1461e93b1ae0eae0a9f0b87a95a2758b0
scuba <scuba75@gmail.com>
2024-11-18 15:13:42 -0500

    update

--- src/arenaSync/cmdQue.js
+++ src/arenaSync/cmdQue.js
@@ -9,7 +9,7 @@ QUE_NAME += `.sync.arena`
 const start = async()=>{
   if(!rabbitmq.ready) return
   let status = await rabbitmq.queueDelete(QUE_NAME)
-  publisher = rabbitmq.createPublisher({ confirm: true, queues: [{ queue: QUE_NAME, durable: true, arguments: { 'x-queue-type': 'quorum', 'x-message-ttl': msgTTL * 1000 } }]})
+  publisher = rabbitmq.createPublisher({ confirm: true, queues: [{ queue: QUE_NAME, durable: true, arguments: { 'x-queue-type': 'quorum' } }]})
   log.info(`${POD_NAME} arena publisher started...`)
   publisherReady = true
   return true
