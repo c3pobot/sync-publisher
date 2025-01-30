6d8e37b5c6446f6141baa3e392742c68107ece7d
scuba75 <scuba75@gmail.com>
2025-01-30 11:34:59 -0500

    update

--- src/arenaSync/cmdQue.js
+++ src/arenaSync/cmdQue.js
@@ -5,10 +5,11 @@ const msgTTL = +process.env.RABBIT_MQ_TTL || 60
 let QUE_NAME = process.env.WORKER_QUE_NAME_SPACE || process.env.NAME_SPACE || 'default', POD_NAME = process.env.POD_NAME || 'sync-publisher', publisher, publisherReady
 QUE_NAME += `.sync.arena`
 
-let queProps = { queue: QUE_NAME, durable: true, arguments: { 'x-queue-type': 'quorum' }  }
+//let queProps = { queue: QUE_NAME, durable: true, arguments: { 'x-queue-type': 'quorum' }  }
+let queProps = { queue: QUE_NAME }
 const start = async()=>{
   if(!rabbitmq.ready) return
-  let status = await rabbitmq.queueDelete(QUE_NAME)
+  //let status = await rabbitmq.queueDelete(QUE_NAME)
   publisher = rabbitmq.createPublisher({ confirm: true, queues: [queProps]})
   log.info(`${POD_NAME} arena publisher started...`)
   publisherReady = true
--- src/arenaSync/sync.js
+++ src/arenaSync/sync.js
@@ -3,15 +3,13 @@ const log = require('logger')
 const mongo = require('mongoclient')
 const cmdQue = require('./cmdQue')
 const exchange = require('./exchange')
-let shardSet = new Set(), patreonSet = new Set(), producerReady, mongoReady
+let producerReady, mongoReady
 
 const checkQue = async()=>{
   let status = await cmdQue.check()
   if(status) return
-  log.info(`Arena Que is empty recreating....`)
-  shardSet = new Set()
-  patreonSet = new Set()
-  await exchange.send('restart')
+  log.debug(`Arena Que is empty re adding....`)
+  return true
 }
 
 const syncPatreon = async()=>{
@@ -20,12 +18,8 @@ const syncPatreon = async()=>{
     let patreons = await mongo.find('patreon', {status: 1}, {_id: 1, status: 1})
     if(!patreons || patreons?.length == 0) return
     for(let i in patreons){
-      if(patreonSet.has(patreons[i]._id)) continue
       let status = await cmdQue.send({ name: 'arena', patreonId: patreons[i]._id})
-      if(status){
-        patreonSet.add(patreons[i]._id)
-        log.debug(`Added ${patreons[i]._id} to patreon que...`)
-      }
+      if(status) log.debug(`Added ${patreons[i]._id} to patreon que...`)
     }
     return true
   }catch(e){
@@ -37,16 +31,9 @@ const syncShards = async()=>{
     if(!producerReady || !mongoReady) return
     let shards = await mongo.find('payoutServers', { status: 1 })
     if(shards?.length == 0) return
-    //await cmdQue.send({ name: 'shard', shardId: '337334829076971521-650381606208405544'})
-    //shardSet.add('337334829076971521-650381606208405544')
-    //return true
     for(let i in shards){
-      if(shardSet.has(shards[i]._id)) continue
       let status = await cmdQue.send({ name: 'shard', shardId: shards[i]._id})
-      if(status){
-        shardSet.add(shards[i]._id)
-        log.debug(`Added ${shards[i]._id} to shard que...`)
-      }
+      if(status) log.debug(`Added ${shards[i]._id} to shard que...`)
     }
     return true
   }catch(e){
@@ -55,13 +42,12 @@ const syncShards = async()=>{
 }
 const sync = async()=>{
   try{
-    let syncTime = 30
-    if(!producerReady) producerReady = cmdQue.status()
-    if(!mongoReady) mongoReady = mongo.status()
-    if(!mongoReady || !producerReady) syncTime = 5
-    await checkQue()
-    await syncShards()
-    await syncPatreon()
+    let syncTime = 5
+    let status = await checkQue()
+    if(status){
+      await syncShards()
+      await syncPatreon()
+    }
     setTimeout(sync, syncTime * 1000)
   }catch(e){
     log.error(e)
